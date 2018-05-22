996ff666b8e79925f79822b4bcb977b14dc9d5c3
Owen Kelly <owen@owenkelly.com.au>
2018-05-22 16:50:52 +1000

    Init of RFC branch

--- examples/readme.md
+++
@@ -1,4 +0,0 @@
-# Examples
-
-This folder contains some example Kombustion stacks. Most of them will require
-you to download a plugin to work.
--- plugins/ian/outputs/aurora.go
+++
@@ -1,9 +0,0 @@
-package outputs
-
-import (
-	"github.com/KablamoOSS/kombustion/types"
-)
-
-func ParseAurora(name string, data string) (cf types.ValueMap, err error) {
-	return
-}
