082e86532d80f12754cb243df6067169fafc55c8
Owen Kelly <owen@owenkelly.com.au>
2018-06-14 14:56:44 +1000

    updated delete

--- internal/tasks/delete.go
+++ internal/tasks/delete.go
@@ -1,18 +1,20 @@
 package tasks
 
 import (
+	"fmt"
+
 	printer "github.com/KablamoOSS/go-cli-printer"
+	"github.com/KablamoOSS/kombustion/internal/cloudformation"
 	"github.com/KablamoOSS/kombustion/internal/cloudformation/tasks"
+	"github.com/KablamoOSS/kombustion/internal/manifest"
 	"github.com/urfave/cli"
 )
 
 // DeleteFlags for use with the delete taks
-var DeleteFlags = []cli.Flag{
-	cli.StringFlag{
-		Name:  "region, r",
-		Usage: "region to delete from",
-		Value: "ap-southeast-2",
-	},
+var DeleteFlags = []cli.Flag{}
+
+func init() {
+	DeleteFlags = append(CloudFormationStackFlags, DeleteFlags...)
 }
 
 // Delete a given stack
@@ -20,5 +22,34 @@ func Delete(c *cli.Context) {
 	printer.Step("Delete stack")
 	printer.Progress("Kombusting")
 
-	tasks.DeleteStack(c.Args().Get(0), c.GlobalString("profile"), c.String("region"))
+	fileName := c.Args().Get(0)
+	if fileName == "" {
+		printer.Fatal(
+			fmt.Errorf("Can't upsert file, no source template provided"),
+			fmt.Sprintf(
+				"Add the path to the source template file you want to generate like: `kombustion upsert template.yaml`.",
+			),
+			"",
+		)
+	}
+
+	manifestFile := manifest.FindAndLoadManifest()
+
+	environment := c.String("environment")
+
+	stackName := cloudformation.GetStackName(manifestFile, fileName, environment, c.String("stack-name"))
+
+	region := c.String("region")
+	if region == "" {
+		// If no region was provided by the cli flag, check for the default in the manifest
+		if manifestFile.Region != "" {
+			region = manifestFile.Region
+		}
+	}
+
+	tasks.DeleteStack(
+		stackName,
+		c.GlobalString("profile"),
+		region,
+	)
 }
